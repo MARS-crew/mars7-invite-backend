4f8551c7e766fd97235c8aa33dc3a6fabcc3be6c
khyu2 <ikeelee7742@gmail.com>
2025-10-13 16:20:32 +0900

    initial commit

---
+++ src/main/java/project/mars7invite/Mars7InviteApplication.java
@@ -0,0 +1,13 @@
+package project.mars7invite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mars7InviteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mars7InviteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/mars7invite/Mars7InviteApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mars7invite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mars7InviteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
