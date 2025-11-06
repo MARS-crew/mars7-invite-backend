0755445cddb1fe322dae38231f50b70ec3a1a77e
junhhhe <129051263+junhhhe@users.noreply.github.com>
2025-11-06 17:45:09 +0900

    forward-headers-strategy 추가
    1. forward-headers-strategy 추가
    2. nginx cors 설정으로 spring cors 설정 제거

--- src/main/java/project/mars7invite/common/config/CorsConfig.java
+++ src/main/java/project/mars7invite/common/config/CorsConfig.java
@@ -4,14 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("https://www.welcome-mars.kro.kr")
-            // .allowedOriginPatterns("*")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("*")
